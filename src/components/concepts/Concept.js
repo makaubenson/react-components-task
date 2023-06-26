89392983eab50cdb0a86e3759fec4e5f4aea37eb
Benson <bensonmakau2000@gmail.com>
2023-06-26 11:24:34 +0300

    rendered data on component

---
+++ src/components/concepts/Concept.js
@@ -0,0 +1,28 @@
+import ConceptItem from "./ConceptItem";
+import "../../index.css";
+
+const Concepts = (props) => {
+  return (
+  <div>
+    <ConceptItem
+      title={props.items[0].title}
+      image={props.items[0].image}
+      description={props.items[0].description}
+    />
+
+    <ConceptItem
+      title={props.items[1].title}
+      image={props.items[1].image}
+      description={props.items[1].description}
+    />
+    <ConceptItem
+      title={props.items[2].title}
+      image={props.items[2].image}
+      description={props.items[2].description}
+    />
+  </div>
+
+  );
+}
+
+export default Concepts;
---
+++ src/components/concepts/ConceptItem.js
@@ -0,0 +1,17 @@
+import "../../index.css"
+
+const ConceptItem = (props) => {
+  return (
+<div>
+  <ul id="concepts">
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  </ul>
+</div>
+  );
+}
+
+export default ConceptItem;
